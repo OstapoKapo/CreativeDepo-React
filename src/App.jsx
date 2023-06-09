a08c20a5a9534aca4c2ea4519060e568f45e05e1
OstapoKapo <OstapoKapo@gmail.com>
2023-06-10 01:10:29 +0300

    products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import React, { useState, useEffect } from "react";
 import './nullstyle.css';
-import Main from './components/Container'
+import Main from './components/Container';
 
 function App() {
+  // const [products, setMessage] = useState("");
+
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/message")
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.products));
+  // }, []);
+
   return (
     <div className="App">
-       <Main />
+     <Main />
     </div>
   );
 }
 
 export default App;
+
