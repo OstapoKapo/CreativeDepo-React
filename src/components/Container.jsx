5bc8f3107f910ae3f38c3fbbef32571ed89575df
OstapoKapo <OstapoKapo@gmail.com>
2023-06-10 14:39:17 +0300

    products

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,6 +2,7 @@ import './Container.css';
 import Header from './header/Header.jsx';
 import Swiper from './Swiper/Swiper';
 import Filter from './Filter/Filter';
+import Prodcuts from './ProductsContainer/ProductsContainer'
 
 function Container() {
   return (
---
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -0,0 +1,11 @@
+import './ProductsContainer.css';
+
+function ProductsContainer() {
+  return (
+        <div className="productsContainer">
+            
+        </div>
+  );
+}
+
+export default ProductsContainer;
