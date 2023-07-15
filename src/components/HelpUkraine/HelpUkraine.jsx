21f74ee2d9a3e28fce4004cf9b2f82eef8ab456b
OstapoKapo <OstapoKapo@gmail.com>
2023-07-15 04:49:27 +0300

    helpUkreine

--- src/components/HelpUkraine/HelpUkraine.jsx
+++ src/components/HelpUkraine/HelpUkraine.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HelpUkraine.css';
 
+
 export default function HelpUkraine() {
   return (
     <div className="helpUkraine">
@@ -13,8 +14,14 @@ export default function HelpUkraine() {
             </div>
         </div>
         <div className="helpUkraine__main">
-            <div className="helpUkraine__main__left"></div>
-            <div className="helpUkraine__main__right"></div>
+            <div className="helpUkraine__main__left">
+                <p className="helpUkraine__header">Як саме ти допомогаєш <br /> Україні, купуючи мерч:</p>
+                <div className="helpUkraine__block helpUkraine__block_blue">Частину прибутку витрачаємо на потреби ЗСУ</div>
+                <div className="helpUkraine__block helpUkraine__block_yellow">Тут є фото бронежилетів, які були виготовлені завдяки вашим замовленням. <br /> <br />  Плити 4 класу, покриті протисколковим та демпферним покриттям. Плитоноска важить 1,5 кг + 2 плити по 3кг, загалом — 7,5 кг, що є досить комфортним для тривалого використання військовими.</div>
+            </div>
+            <div className="helpUkraine__main__right">
+                
+            </div>
         </div>
     </div>
   )
